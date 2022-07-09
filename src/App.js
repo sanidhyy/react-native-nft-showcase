5d4f02edd972533fab78b115536062dfadab17d9
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-09 15:58:53 +0530

    banners added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,20 @@ const App = () => {
       />
 
       <Features />
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
     </>
   );
 };
