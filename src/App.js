73b326a374455220fb8d28e810c8c563770f4039
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-09 12:01:05 +0530

    tailwindcss initial setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
